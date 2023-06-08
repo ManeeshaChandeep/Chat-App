c55d37fa1310c6f03e5202be8c871c36a69e3862
ManeeshaChandeep <maneeshachandeep@gmail.com>
2023-06-09 01:32:15 +0530

    server and multi client handler

--- Server/src/util/MultiClientHandler.java
+++ Server/src/util/MultiClientHandler.java
@@ -1,4 +1,102 @@
 package util;
 
-public class MultiClientHandler {
+import java.io.*;
+import java.net.Socket;
+
+public class MultiClientHandler extends Thread{
+    private Socket clientSocket;
+    DataOutputStream dataOutputStream;
+    String userName;
+
+    public MultiClientHandler(Socket socket,String userName) {
+        this.clientSocket = socket;
+        this.userName=userName;
+    }
+
+    public void run() {
+        try {
+            dataOutputStream=new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+
+            while (true){
+
+                String dataType = dataInputStream.readUTF();
+                if (dataType.equals("TEXT")){
+                    String message =userName+" : "+ dataInputStream.readUTF();
+
+                    if (message.equalsIgnoreCase("CLOSE_CHAT")){
+                        break;
+                    }
+
+                    Server.broadcastMessage(this.userName,message);
+                }else {
+                    String imageFilePath = "E:\\IJSE\\IT2\\ChatApp\\ClientSide2\\src\\data\\images\\image.jpg";
+                    File receivedImage = new File(imageFilePath);
+
+                    try (FileOutputStream fileOutputStream = new FileOutputStream(receivedImage)) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+
+                        while ((bytesRead = dataInputStream.read(buffer)) != -1) {
+                            fileOutputStream.write(buffer, 0, bytesRead);
+
+                            if (bytesRead < buffer.length) {
+                                break;
+                            }
+                        }
+                        Server.broadcastImage(this.userName,receivedImage);
+
+                    } catch (IOException ex) {
+                        System.out.println("Error saving the image: " + ex.getMessage());
+                    }
+
+                }
+
+            }
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void sendMessage(String reply) {
+        try {
+            //Setting the data type to the clients
+            dataOutputStream.writeUTF("TEXT");
+            dataOutputStream.flush();
+
+            dataOutputStream.writeUTF(reply);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendImages(File file){
+        try {
+            //Setting the data type to the clients
+            dataOutputStream.writeUTF("IMAGE");
+            dataOutputStream.flush();
+
+
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            // Send the image data to the server
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                dataOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            dataOutputStream.flush();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
+
--- Server/src/util/Server.java
+++ Server/src/util/Server.java
@@ -1,5 +1,54 @@
 package util;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
+
+    private static List<MultiClientHandler> clients = new ArrayList<>();
+    public static void main(String[] args) {
+        final int port = 3000;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server started. Listening on port " + port);
+
+            for (int i = 0; i < 5; i++) {
+                Socket clientSocket = serverSocket.accept();
+
+                DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
+                String clientName = dataInputStream.readUTF();
+                System.out.println("Client connected: " + clientName);
+
+                MultiClientHandler clientHandler = new MultiClientHandler(clientSocket,clientName);
+                clients.add(clientHandler);
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void broadcastMessage(String name,String message) {
+        for (MultiClientHandler client : clients) {
+            if (!client.userName.equals(name)){
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    public static void broadcastImage(String name, File file) {
+        for (MultiClientHandler client : clients) {
+            if (!client.userName.equals(name)){
+                client.sendImages(file);
+            }
+        }
+    }
+
 
 }
